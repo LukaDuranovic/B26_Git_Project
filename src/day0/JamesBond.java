23d93fa06990af310f7cef50de8be439610fd83a
Luka Duranovic <98862500+LukaDuranovic@users.noreply.github.com>
2022-03-26 13:32:47 -0700

    Update JamesBond.java

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -4,7 +4,7 @@ public class JamesBond
 {
   public static void main(String[] args)
   {
-    System.our.println("James Bond hacked the project");
+    System.out.println("James Bond hacked the project");
   }
 }
   
